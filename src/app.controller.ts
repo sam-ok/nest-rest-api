9a2017063f94cc3e427ad63ce3e22b23dde45eb0
Sam O <sam.ictdev@gmail.com>
2021-03-04 10:30:48 +0300

    update controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('pdt')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('1')
+  /*The getHello metho is invoked whenever the / endpoint is reached.*/
   getHello(): string {
     return this.appService.getHello();
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,17 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
+  /* Controllers control how we handle incoming request.
+  They are responsible for accepting incoming requests, 
+  doing something with those requests and sending
+  back responses.
+
+  Providers are extra classes that can be injected 
+  into controllers or other providers to provide 
+  extra services such as getting data from a database.
+  */
   controllers: [AppController],
   providers: [AppService],
 })
+/* The module bundles up the different features in the app. */
 export class AppModule {}
